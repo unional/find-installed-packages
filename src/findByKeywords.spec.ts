fe3eacd59801a002a15f420aefa2cd5b11b62afe
Homa Wong <homawong@gmail.com>
2019-02-07 00:08:52 -0800

    feat: change findByKeyword to findByKeywords (#5)
    * feat: change findByKeyword to findByKeywords
    
    BREAKING CHANGE: findByKeyword removed.

---
+++ src/findByKeywords.spec.ts
@@ -0,0 +1,69 @@
+import { findByKeywords } from '.';
+
+describe('find local', () => {
+  test('not exist folder returns empty array', async () => {
+    const actual = await findByKeywords(['not-exist-keyword'], { cwd: '../fixtures/not-exist' })
+    expect(actual).toEqual([])
+  })
+
+  test('no node_modues folder returns empty array', async () => {
+    const actual = await findByKeywords(['not-exist-keyword'], { cwd: '../fixtures/no-node_modules' })
+    expect(actual).toEqual([])
+  })
+
+  test('no package containing keyword returns empty array', async () => {
+    const actual = await findByKeywords(['some-keyword'], { cwd: 'fixtures/no-keyword' })
+    expect(actual).toEqual([])
+  })
+
+  test('find package containing specified keyword', async () => {
+    const actual = await findByKeywords(['some-keyword'], { cwd: 'fixtures/one-plugin' })
+    expect(actual).toEqual(['plugin-a'])
+  })
+
+  test('package does not contain all keywords do not return', async () => {
+    const actual = await findByKeywords(['some-keyword', 'another-keyword'], { cwd: 'fixtures/one-plugin' })
+    expect(actual).toEqual([])
+  })
+
+  test('find package contains all keywords', async () => {
+    const actual = await findByKeywords(['some-keyword', 'another-keyword'], { cwd: 'fixtures/multi-keywords' })
+    expect(actual).toEqual(['plugin-m'])
+  })
+
+  test('find scoped package containing specified keyword', async () => {
+    const actual = await findByKeywords(['some-keyword'], { cwd: 'fixtures/scoped-one-plugin' })
+    expect(actual).toEqual(['@some-scope/plugin-a'])
+  })
+
+  test('scoped package does not contain all keywords do not return', async () => {
+    const actual = await findByKeywords(['some-keyword', 'another-keyword'], { cwd: 'fixtures/scoped-one-plugin' })
+    expect(actual).toEqual([])
+  })
+
+  test('find package in nested node_modules', async () => {
+    const actual = await findByKeywords(['some-keyword'], { cwd: 'fixtures/nested-one-plugin' })
+    expect(actual).toEqual(['plugin-a'])
+  })
+
+  test('find scoped package in nested node_modules', async () => {
+    const actual = await findByKeywords(['some-keyword'], { cwd: 'fixtures/nested-scoped-one-plugin' })
+    expect(actual).toEqual(['@some-scope/plugin-a'])
+  })
+
+  test('@types is ignored', async () => {
+    const actual = await findByKeywords(['some-keyword'], { cwd: 'fixtures/at-types-plugin' })
+    expect(actual).toEqual([])
+  })
+
+  test('should not get package not under top node_modules hierarchy', async () => {
+    const packagesInfo = await findByKeywords(['some-keyword'])
+
+    expect(packagesInfo).toEqual([])
+  })
+
+  test('ignore files under node_modules', async () => {
+    const packagesInfo = await findByKeywords(['some'], { cwd: 'fixtures/node_modules-with-file' })
+    expect(packagesInfo.length).toBe(0)
+  })
+})
