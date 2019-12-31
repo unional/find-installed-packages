de1f2d2ddf08675c177a422e749e47a34cda017c
Homa Wong <homawong@gmail.com>
2019-12-30 23:18:05 -0800

    fix: check only valid package folders (#13)

--- src/findPackagesInfo.spec.ts
+++ src/findPackagesInfo.spec.ts
@@ -1,9 +1,14 @@
 import a from 'assertron'
 import { findPackagesInfo } from './findPackagesInfo'
 
-test('node_modules in test folder will not take into consideration', async () => {
-  const packagesInfo = await findPackagesInfo('fixtures/node_modules-in-test')
+test('node_modules in test folder will not take into consideration', () => {
+  const packagesInfo = findPackagesInfo('fixtures/node_modules-in-test')
 
   a.satisfies(packagesInfo, [{ name: 'pkg-with-test' }])
-  expect(packagesInfo.length).toBe(1)
+})
+
+test('folder without node_modules will not considered', () => {
+  const packagesInfo = findPackagesInfo('fixtures/no-node_modules')
+
+  expect(packagesInfo.length).toBe(0)
 })
