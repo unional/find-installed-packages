489c8ef329c782f38480ef88ae547bf30287ad9a
Homa Wong <homawong@gmail.com>
2022-09-07 15:06:28 -0700

    fix: skip over permission denied
    When using `pnpm`,
    I encountered a case where `fs.readdirSync(nodeModulesDir)` failed with permission denied.
    
    It issue is likely related to recursion -> infinite loop,
    due to symlink.
    
    So when that occurs, ignoring that folder and move on.

--- src/findPackagesInfo.ts
+++ src/findPackagesInfo.ts
@@ -12,28 +12,35 @@ function recurseFind(cwd: string) {
   const nodeModulesDir = path.join(cwd, 'node_modules')
   if (!fs.existsSync(nodeModulesDir)) return []
 
-  const dirs = fs.readdirSync(nodeModulesDir)
-  return dirs.reduce((p, dir) => {
-    const packagePath = path.join(nodeModulesDir, dir)
-    const stat = fs.statSync(packagePath)
-    if (!stat.isDirectory()) return p
-    if (dir.startsWith('@')) {
-      if (dir === '@types') return p
-      const subDirs = fs.readdirSync(path.join(nodeModulesDir, dir))
-      subDirs.forEach(sd => {
-        const packagePath = path.join(nodeModulesDir, dir, sd)
-        const stat = fs.statSync(packagePath)
-        // istanbul ignore next
-        if (!stat.isDirectory()) return
-        p.push({ name: `${dir}/${sd}`, ctimeMs: stat.ctimeMs, path: packagePath })
+  try {
+    const dirs = fs.readdirSync(nodeModulesDir)
+    return dirs.reduce((p, dir) => {
+      const packagePath = path.join(nodeModulesDir, dir)
+      const stat = fs.statSync(packagePath)
+      if (!stat.isDirectory()) return p
+      if (dir.startsWith('@')) {
+        if (dir === '@types') return p
+        const subDirs = fs.readdirSync(path.join(nodeModulesDir, dir))
+        subDirs.forEach(sd => {
+          const packagePath = path.join(nodeModulesDir, dir, sd)
+          const stat = fs.statSync(packagePath)
+          // istanbul ignore next
+          if (!stat.isDirectory()) return
+          p.push({ name: `${dir}/${sd}`, ctimeMs: stat.ctimeMs, path: packagePath })
+          p.push(...recurseFind(packagePath))
+        })
+      }
+      else {
+        p.push({ name: dir, ctimeMs: stat.ctimeMs, path: packagePath })
         p.push(...recurseFind(packagePath))
-      })
-    }
-    else {
-      p.push({ name: dir, ctimeMs: stat.ctimeMs, path: packagePath })
-      p.push(...recurseFind(packagePath))
-    }
+      }
 
-    return p
-  }, [] as PackageInfo[])
+      return p
+    }, [] as PackageInfo[])
+  }
+  catch {
+    // e.g. permission denied
+    // istanbul ignore next
+    return []
+  }
 }
