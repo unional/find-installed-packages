8783ed38243844f02cc0d1a092d357490270da2e
Homa Wong <homawong@gmail.com>
2022-09-07 21:56:58 -0700

    fix: improve recursive case
    Will continue to go through remaining dirs if one of them fail,
    and track tranversed path to quit if we get to an recursive loop

--- src/findPackagesInfo.ts
+++ src/findPackagesInfo.ts
@@ -5,42 +5,54 @@ export type PackageInfo = { name: string, ctimeMs: number, path: string }
 export function findPackagesInfo(cwd: string): PackageInfo[] {
   if (!fs.existsSync(cwd)) return []
 
-  return recurseFind(cwd)
+  return recurseFind(cwd, {})
 }
 
-function recurseFind(cwd: string) {
+function recurseFind(cwd: string, walked: Record<string, true>) {
   const nodeModulesDir = path.join(cwd, 'node_modules')
   if (!fs.existsSync(nodeModulesDir)) return []
+  if (walked[nodeModulesDir]) return []
 
+  const dirs = fs.readdirSync(nodeModulesDir)
+  walked[nodeModulesDir] = true
+  return dirs.reduce((p, dir) => {
+    const packagePath = path.join(nodeModulesDir, dir)
+    const stat = tryStatSync(packagePath)
+    if (!stat) return p
+    if (!stat.isDirectory()) return p
+    walked[packagePath] = true
+    if (dir.startsWith('@')) {
+      if (dir === '@types') return p
+      const subDirs = fs.readdirSync(packagePath)
+      subDirs.forEach(sd => {
+        const packagePath = path.join(nodeModulesDir, dir, sd)
+        if (walked[packagePath]) return
+        const stat = tryStatSync(packagePath)
+        // istanbul ignore next
+        if (!stat) return
+        // istanbul ignore next
+        if (!stat.isDirectory()) return
+        walked[packagePath] = true
+        p.push({ name: `${dir}/${sd}`, ctimeMs: stat.ctimeMs, path: packagePath })
+        p.push(...recurseFind(packagePath, walked))
+      })
+    }
+    else {
+      p.push({ name: dir, ctimeMs: stat.ctimeMs, path: packagePath })
+      p.push(...recurseFind(packagePath, walked))
+    }
+
+    return p
+  }, [] as PackageInfo[])
+}
+
+function tryStatSync(path: string) {
   try {
-    const dirs = fs.readdirSync(nodeModulesDir)
-    return dirs.reduce((p, dir) => {
-      const packagePath = path.join(nodeModulesDir, dir)
-      const stat = fs.statSync(packagePath)
-      if (!stat.isDirectory()) return p
-      if (dir.startsWith('@')) {
-        if (dir === '@types') return p
-        const subDirs = fs.readdirSync(path.join(nodeModulesDir, dir))
-        subDirs.forEach(sd => {
-          const packagePath = path.join(nodeModulesDir, dir, sd)
-          const stat = fs.statSync(packagePath)
-          // istanbul ignore next
-          if (!stat.isDirectory()) return
-          p.push({ name: `${dir}/${sd}`, ctimeMs: stat.ctimeMs, path: packagePath })
-          p.push(...recurseFind(packagePath))
-        })
-      }
-      else {
-        p.push({ name: dir, ctimeMs: stat.ctimeMs, path: packagePath })
-        p.push(...recurseFind(packagePath))
-      }
-
-      return p
-    }, [] as PackageInfo[])
+    return fs.statSync(path)
   }
   catch {
-    // e.g. permission denied
+    // it fails in Node 14 sometimes, with permission denied.
     // istanbul ignore next
-    return []
+    return undefined
   }
 }
